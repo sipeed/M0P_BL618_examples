cbd3253c24779a2ce23962929df31a46d8da140b
taorye <taorye@outlook.com>
2023-04-04 16:15:30 +0800

    feat(m0pdock): product testing or out of factory
    1. the lcd pannel should display something
    2. the camera picture should be displayed on the screen if there is camera
    3. the buttons above should respond to your touch
    4. voice captured by the MIC should show on screen as a curve of change
    5. the two side keys should control the `led` on the screen

---
+++ sipeed_support/examples/m0pdock/pt/main.c
@@ -0,0 +1,333 @@
+#include "board.h"
+#include "bflb_gpio.h"
+#include "bflb_l1c.h"
+#include "bflb_mtimer.h"
+
+#include "lv_conf.h"
+#include "lvgl.h"
+
+#include "lv_port_disp.h"
+#include "lv_port_indev.h"
+
+#include "pt/pt.h"
+
+#include "lcd.h"
+
+#include "bflb_adc.h"
+#include "bflb_cam.h"
+#include "bflb_dma.h"
+#include "csi_rv32_gcc.h"
+#include "image_sensor.h"
+
+#define DBG_TAG "MAIN"
+#include "log.h"
+
+static struct bflb_device_s *cam0;
+static struct bflb_device_s *adc;
+
+static int filesystem_init(void);
+static void cam_init(void);
+static void adc_init(void);
+
+int main(void)
+{
+    board_init();
+
+    label_tfcard_state_update(!filesystem_init());
+
+    struct bflb_device_s *gpio;
+    gpio = bflb_device_get_by_name("gpio");
+    /* backlight pin */
+    bflb_gpio_init(gpio, GPIO_PIN_2, GPIO_OUTPUT | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_0);
+    bflb_gpio_set(gpio, GPIO_PIN_2);
+
+    /* lvgl init */
+    lv_log_register_print_cb(NULL);
+    lv_init();
+    lv_port_disp_init();
+    lv_port_indev_init();
+
+    pt();
+
+    /* ADC_CH0 */
+    bflb_gpio_init(gpio, GPIO_PIN_20, GPIO_ANALOG | GPIO_SMT_EN | GPIO_DRV_0);
+    /* ADC_CH3 */
+    bflb_gpio_init(gpio, GPIO_PIN_3, GPIO_ANALOG | GPIO_SMT_EN | GPIO_DRV_0);
+    /* adc init */
+    adc_init();
+
+    /* DVP0 GPIO init */
+    /* I2C GPIO */
+    // bflb_gpio_init(gpio, GPIO_PIN_0, GPIO_FUNC_I2C0 | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    // bflb_gpio_init(gpio, GPIO_PIN_1, GPIO_FUNC_I2C0 | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+
+    /* Power down GPIO */
+    bflb_gpio_init(gpio, GPIO_PIN_16, GPIO_OUTPUT | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    bflb_gpio_reset(gpio, GPIO_PIN_16);
+
+    /* MCLK GPIO */
+    bflb_gpio_init(gpio, GPIO_PIN_6, GPIO_FUNC_CLKOUT | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+
+    /* DVP0 GPIO */
+    bflb_gpio_init(gpio, GPIO_PIN_24, GPIO_FUNC_CAM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    bflb_gpio_init(gpio, GPIO_PIN_25, GPIO_FUNC_CAM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    bflb_gpio_init(gpio, GPIO_PIN_26, GPIO_FUNC_CAM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    bflb_gpio_init(gpio, GPIO_PIN_27, GPIO_FUNC_CAM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    bflb_gpio_init(gpio, GPIO_PIN_28, GPIO_FUNC_CAM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    bflb_gpio_init(gpio, GPIO_PIN_29, GPIO_FUNC_CAM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    bflb_gpio_init(gpio, GPIO_PIN_30, GPIO_FUNC_CAM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    bflb_gpio_init(gpio, GPIO_PIN_31, GPIO_FUNC_CAM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    bflb_gpio_init(gpio, GPIO_PIN_32, GPIO_FUNC_CAM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    bflb_gpio_init(gpio, GPIO_PIN_33, GPIO_FUNC_CAM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    bflb_gpio_init(gpio, GPIO_PIN_34, GPIO_FUNC_CAM | GPIO_ALTERNATE | GPIO_PULLUP | GPIO_SMT_EN | GPIO_DRV_1);
+    cam_init();
+
+    LOG_I("start loop\r\n");
+
+    while (1) {
+        lv_task_handler();
+        bflb_mtimer_delay_ms(1);
+    }
+}
+
+#include "ff.h"
+#include "fatfs_diskio_register.h"
+
+static int filesystem_init(void)
+{
+    static FATFS fs;
+    static __attribute((aligned(8))) uint32_t workbuf[4 * 1024];
+
+    MKFS_PARM fs_para = {
+        .fmt = FM_FAT32,     /* Format option (FM_FAT, FM_FAT32, FM_EXFAT and FM_SFD) */
+        .n_fat = 1,          /* Number of FATs */
+        .align = 0,          /* Data area alignment (sector) */
+        .n_root = 1,         /* Number of root directory entries */
+        .au_size = 512 * 32, /* Cluster size (byte) */
+    };
+
+    FRESULT ret;
+
+    board_sdh_gpio_init();
+
+    fatfs_sdh_driver_register();
+
+    ret = f_mount(&fs, "/sd", 1);
+
+    if (ret == FR_NO_FILESYSTEM) {
+        LOG_W("No filesystem yet, try to be formatted...\r\n");
+
+        ret = f_mkfs("/sd", &fs_para, workbuf, sizeof(workbuf));
+
+        if (ret != FR_OK) {
+            LOG_F("fail to make filesystem\r\n");
+            return ret;
+        }
+
+        if (ret == FR_OK) {
+            LOG_I("done with formatting.\r\n");
+            LOG_I("first start to unmount.\r\n");
+            ret = f_mount(NULL, "/sd", 1);
+            LOG_I("then start to remount.\r\n");
+        }
+    } else if (ret != FR_OK) {
+        LOG_F("fail to mount filesystem,error= %d\r\n", ret);
+        LOG_F("SD card might fail to initialise.\r\n");
+        return ret;
+    } else {
+        LOG_D("Succeed to mount filesystem\r\n");
+    }
+
+    if (ret == FR_OK) {
+        LOG_I("FileSystem cluster size:%d-sectors (%d-Byte)\r\n", fs.csize, fs.csize * 512);
+    }
+
+    return ret;
+}
+
+static void cam_isr(int irq, void *arg);
+
+static void cam_init(void)
+{
+    struct bflb_cam_config_s cam_config;
+    struct image_sensor_config_s *sensor_config;
+    struct bflb_device_s *i2c0;
+
+    i2c0 = bflb_device_get_by_name("i2c0");
+    cam0 = bflb_device_get_by_name("cam0");
+
+    if (image_sensor_scan(i2c0, &sensor_config)) {
+        LOG_I("\r\nSensor name: %s\r\n", sensor_config->name);
+    } else {
+        LOG_E("\r\nError! Can't identify sensor!\r\n");
+        cam0 = NULL;
+        return;
+    }
+
+    bflb_cam_int_mask(cam0, CAM_INTMASK_NORMAL, false);
+    bflb_irq_attach(cam0->irq_num, cam_isr, NULL);
+    bflb_irq_enable(cam0->irq_num);
+
+    memcpy(&cam_config, sensor_config, IMAGE_SENSOR_INFO_COPY_SIZE);
+    cam_config.with_mjpeg = false;
+    cam_config.output_format = CAM_OUTPUT_FORMAT_AUTO;
+    static lv_color_t cam_buffer[4][320 * 240] __section(".psmram_data");
+    cam_config.output_bufaddr = (uint32_t)(uintptr_t)(void *)cam_buffer;
+    cam_config.output_bufsize = sizeof(cam_buffer);
+
+    bflb_cam_init(cam0, &cam_config);
+    bflb_cam_start(cam0);
+
+    // bflb_cam_stop(cam0);
+}
+
+#define UPDATE_FREQ       2
+#define TEST_COUNT        64
+#define TEST_ADC_CHANNELS 2
+
+static ATTR_NOCACHE_NOINIT_RAM_SECTION uint32_t adc_raw_data[2][TEST_ADC_CHANNELS * TEST_COUNT];
+static void dma0_ch0_isr(void *arg);
+
+static void adc_init(void)
+{
+    struct bflb_adc_channel_s chan[] = {
+        { .pos_chan = ADC_CHANNEL_0,
+          .neg_chan = ADC_CHANNEL_GND },
+        { .pos_chan = ADC_CHANNEL_3,
+          .neg_chan = ADC_CHANNEL_GND },
+    };
+
+    adc = bflb_device_get_by_name("adc");
+
+    /**
+     *  adc clock = XCLK / 2 / 20 / 64(14B) = 15.625K
+     */
+    struct bflb_adc_config_s cfg;
+    cfg.clk_div = ADC_CLK_DIV_20;
+    cfg.scan_conv_mode = true;
+    cfg.continuous_conv_mode = true;
+    cfg.differential_mode = false;
+    cfg.resolution = ADC_RESOLUTION_14B;
+    cfg.vref = ADC_VREF_2P0V;
+
+    bflb_adc_init(adc, &cfg);
+    bflb_adc_channel_config(adc, chan, sizeof(chan) / sizeof(chan[0]));
+    bflb_adc_link_rxdma(adc, true);
+
+    struct bflb_device_s *dma0_ch0;
+    dma0_ch0 = bflb_device_get_by_name("dma0_ch0");
+
+    struct bflb_dma_channel_config_s config;
+
+    config.direction = DMA_PERIPH_TO_MEMORY;
+    config.src_req = DMA_REQUEST_ADC;
+    config.dst_req = DMA_REQUEST_NONE;
+    config.src_addr_inc = DMA_ADDR_INCREMENT_DISABLE;
+    config.dst_addr_inc = DMA_ADDR_INCREMENT_ENABLE;
+    config.src_burst_count = DMA_BURST_INCR1;
+    config.dst_burst_count = DMA_BURST_INCR1;
+    config.src_width = DMA_DATA_WIDTH_32BIT;
+    config.dst_width = DMA_DATA_WIDTH_32BIT;
+    bflb_dma_channel_init(dma0_ch0, &config);
+
+    bflb_dma_channel_irq_attach(dma0_ch0, dma0_ch0_isr, NULL);
+
+    static struct bflb_dma_channel_lli_pool_s lli[20]; /* max trasnfer size 4064 * 20 */
+    static struct bflb_dma_channel_lli_transfer_s transfers[2];
+
+    transfers[0].src_addr = (uint32_t)DMA_ADDR_ADC_RDR;
+    transfers[0].dst_addr = (uint32_t)adc_raw_data[0];
+    transfers[0].nbytes = sizeof(adc_raw_data[0]);
+
+    transfers[1].src_addr = (uint32_t)DMA_ADDR_ADC_RDR;
+    transfers[1].dst_addr = (uint32_t)adc_raw_data[1];
+    transfers[1].nbytes = sizeof(adc_raw_data[1]);
+
+    int used_count = bflb_dma_channel_lli_reload(dma0_ch0, lli, 20, transfers, 2);
+    bflb_dma_channel_lli_link_head(dma0_ch0, lli, used_count);
+    bflb_dma_channel_start(dma0_ch0);
+
+    // bflb_adc_start_conversion(adc);
+
+    bflb_adc_stop_conversion(adc);
+}
+
+static void cam_isr(int irq, void *arg)
+{
+    uint16_t *pic_addr;
+    uint32_t pic_size;
+    // static volatile uint32_t cam_int_cnt = 0;
+
+    bflb_cam_int_clear(cam0, CAM_INTCLR_NORMAL);
+    pic_size = bflb_cam_get_frame_info(cam0, (void *)&pic_addr);
+    bflb_cam_pop_one_frame(cam0);
+    // LOG_I("CAM interrupt, pop picture %d: 0x%08x, len: %d\r\n", cam_int_cnt++, (uint32_t)pic_addr, pic_size);
+    for (size_t i = 0; i < pic_size / sizeof(uint16_t); i++) {
+        pic_addr[i] = __bswap16(pic_addr[i]);
+    }
+    canvas_cam_update(pic_addr);
+}
+
+static void dma0_ch0_isr(void *arg)
+{
+    static uint32_t dma_tc_flag0 = 0;
+    dma_tc_flag0++;
+    // LOG_I("[%d]tc done\r\n", dma_tc_flag0);
+
+    struct bflb_adc_result_s result[TEST_ADC_CHANNELS * TEST_COUNT];
+    bflb_adc_parse_result(adc, adc_raw_data[!(dma_tc_flag0 & 0x1)], result, TEST_ADC_CHANNELS * TEST_COUNT);
+
+    int16_t results_temp[TEST_COUNT];
+    uint32_t btn_adc_val_avg = 0;
+    for (size_t j = 0, k = 0; j < TEST_ADC_CHANNELS * TEST_COUNT; j++) {
+        // printf("raw data:%08x\r\n", adc_raw_data[!(dma_tc_flag0 & 0x1)][j]);
+        // ADC_CHANNEL_0 min:923mv nor:952mv max:980mv
+        // printf("pos chan %d,%d mv \r\n", result[j].pos_chan, result[j].millivolt);
+        if (ADC_CHANNEL_3 == result[j].pos_chan) {
+            btn_adc_val_avg += result[j].millivolt;
+        } else if (ADC_CHANNEL_0 == result[j].pos_chan) {
+            results_temp[k++] = result[j].millivolt; // - 952;
+        }
+    }
+    btn_adc_val_avg /= TEST_COUNT;
+
+    chart_mic_append_data(results_temp, TEST_COUNT);
+    label_adc_btn_update(btn_adc_val_avg);
+}
+
+void btn_cam_event_handled(lv_event_t *e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+
+    lv_obj_t *btn = e->target;
+    // lv_label_t *label = (lv_label_t *)lv_obj_get_child(btn, 0);
+
+    bool is_btn_checked = (lv_obj_get_state(btn) & LV_STATE_CHECKED);
+
+    if (code == LV_EVENT_VALUE_CHANGED) {
+        // LV_LOG_USER("%s checked %u", label->text, is_btn_checked);
+        if (cam0) {
+            (void (*[])(struct bflb_device_s *)){ bflb_cam_stop, bflb_cam_start }[is_btn_checked](cam0);
+        }
+    }
+}
+
+void btn_adc_event_handled(lv_event_t *e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+
+    lv_obj_t *btn = e->target;
+    lv_label_t *label = (lv_label_t *)lv_obj_get_child(btn, 0);
+
+    bool is_btn_checked = (lv_obj_get_state(btn) & LV_STATE_CHECKED);
+
+    if (code == LV_EVENT_VALUE_CHANGED) {
+        // LV_LOG_USER("%s checked %u", label->text, is_btn_checked);
+        if (adc) {
+            (void (*[])(struct bflb_device_s *)){ bflb_adc_stop_conversion, bflb_adc_start_conversion }[is_btn_checked](adc);
+            if (!is_btn_checked) {
+                lv_label_set_text((lv_obj_t *)label, "ADC");
+            }
+        }
+    }
+}
---
+++ sipeed_support/examples/m0pdock/pt/src/pt.c
@@ -0,0 +1,167 @@
+#include "pt/pt.h"
+
+static lv_obj_t *canvas_cam;
+static lv_obj_t *chart_mic;
+static lv_obj_t *label_tfcard;
+static lv_obj_t *label_adc_btn;
+static lv_obj_t *led_adc_btn[2];
+
+static bool label_tfcard_state;
+static lv_coord_t ecg_sample[160];
+
+static lv_obj_t *canvas_cam_create(lv_obj_t *parent);
+static lv_obj_t *chart_mic_create(lv_obj_t *parent);
+static lv_obj_t *label_btns_leds_create(lv_obj_t *parent);
+
+void pt(void)
+{
+    canvas_cam = canvas_cam_create(lv_scr_act());
+    chart_mic = chart_mic_create(lv_scr_act());
+    label_tfcard = label_btns_leds_create(chart_mic);
+
+    if (label_tfcard_state) {
+        lv_label_set_text(label_tfcard, "TFCard Plugged!");
+        lv_obj_set_style_text_color(label_tfcard, lv_color_make(0x00, 0x00, 0xff), 0);
+    } else {
+        lv_label_set_text(label_tfcard, "TFCard not found!");
+        lv_obj_set_style_text_color(label_tfcard, lv_color_make(0xff, 0x00, 0x00), 0);
+    }
+}
+
+void canvas_cam_update(void *pic_addr)
+{
+    if (!canvas_cam) {
+        return;
+    }
+
+    lv_obj_t *canvas = canvas_cam;
+    lv_canvas_set_buffer(canvas, pic_addr, 320, 240, LV_IMG_CF_TRUE_COLOR);
+}
+
+void chart_mic_append_data(int16_t *data, uint16_t len)
+{
+    uint32_t pcnt = sizeof(ecg_sample) / sizeof(ecg_sample[0]);
+    if (!chart_mic || !data || !len) {
+        return;
+    }
+    if (len > pcnt)
+        len = pcnt;
+
+    lv_obj_t *chart = chart_mic;
+    lv_chart_set_point_count(chart, 0);
+
+    memcpy(ecg_sample, ecg_sample + len, (pcnt - len) * sizeof(lv_coord_t));
+    memcpy(ecg_sample + pcnt - len, data, len * sizeof(lv_coord_t));
+
+    lv_chart_set_point_count(chart, pcnt);
+}
+
+void label_tfcard_state_update(bool en)
+{
+    label_tfcard_state = en;
+}
+
+void label_adc_btn_update(uint16_t val)
+{
+    if (!label_adc_btn) {
+        return;
+    }
+
+    if ((lv_obj_get_state(lv_obj_get_parent(label_adc_btn)) & LV_STATE_CHECKED)) {
+        lv_label_set_text_fmt(label_adc_btn, "%-4u mv", val);
+    }
+
+    bool btn_clicked[2];
+
+    bool both_clicked = val < 31;
+    if (both_clicked) {
+        btn_clicked[0] = true;
+        btn_clicked[1] = true;
+    } else {
+        btn_clicked[0] = val >= 700 && val < 800;
+        btn_clicked[1] = val >= 31 && val < 50;
+    }
+
+    bool led_states[2];
+    for (uint32_t i = 0; i < 2; i++) {
+        led_states[i] = btn_clicked[i];
+        // ((void (*[])(lv_obj_t *)){ lv_led_off, lv_led_on }[led_states[i]])(led_adc_btn[i]);
+        lv_led_set_color(led_adc_btn[i], (lv_color_t[]){ lv_color_black(), lv_color_make(0xff, 0x00, 0x00) }[led_states[i]]);
+    }
+}
+
+static lv_obj_t *canvas_cam_create(lv_obj_t *parent)
+{
+    lv_obj_t *canvas;
+    canvas = lv_canvas_create(parent);
+    lv_obj_set_size(canvas, 320, 240);
+    lv_obj_align(canvas, LV_ALIGN_TOP_MID, 0, 0);
+
+    return canvas;
+}
+
+static lv_obj_t *chart_mic_create(lv_obj_t *parent)
+{
+    /*Create a chart*/
+    lv_obj_t *chart;
+    chart = lv_chart_create(parent);
+    lv_obj_set_size(chart, 320, 80);
+    lv_obj_align(chart, LV_ALIGN_BOTTOM_MID, 0, 0);
+    lv_chart_set_range(chart, LV_CHART_AXIS_PRIMARY_Y, 0, 2000);
+
+    /*Do not display points on the data*/
+    lv_obj_set_style_size(chart, 0, LV_PART_INDICATOR);
+
+    lv_chart_series_t *ser = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_RED), LV_CHART_AXIS_PRIMARY_Y);
+    LV_ASSERT(ser == lv_chart_get_series_next(chart, NULL));
+
+    uint32_t pcnt = sizeof(ecg_sample) / sizeof(ecg_sample[0]);
+    lv_chart_set_point_count(chart, pcnt);
+    lv_chart_set_ext_y_array(chart, lv_chart_get_series_next(chart, NULL), (lv_coord_t *)ecg_sample);
+
+    lv_chart_set_zoom_x(chart, LV_IMG_ZOOM_NONE);
+    lv_chart_set_zoom_y(chart, LV_IMG_ZOOM_NONE);
+
+    return chart;
+}
+
+static lv_obj_t *label_btns_leds_create(lv_obj_t *parent)
+{
+    lv_obj_t *label;
+
+    lv_obj_t *btn;
+    btn = lv_btn_create(parent);
+    lv_obj_add_flag(btn, LV_OBJ_FLAG_CHECKABLE);
+    lv_obj_add_state(btn, LV_STATE_CHECKED);
+    lv_obj_set_height(btn, LV_SIZE_CONTENT);
+    label = lv_label_create(btn);
+    lv_obj_center(label);
+    lv_obj_add_event_cb(btn, btn_cam_event_handled, LV_EVENT_ALL, NULL);
+    lv_obj_align(btn, LV_ALIGN_BOTTOM_LEFT, -10, 0);
+    lv_label_set_text(label, "CAM");
+
+    btn = lv_btn_create(parent);
+    lv_obj_add_flag(btn, LV_OBJ_FLAG_CHECKABLE);
+    lv_obj_set_height(btn, LV_SIZE_CONTENT);
+    label = lv_label_create(btn);
+    label_adc_btn = label;
+    lv_obj_center(label);
+    lv_obj_add_event_cb(btn, btn_adc_event_handled, LV_EVENT_ALL, NULL);
+    lv_obj_align(btn, LV_ALIGN_BOTTOM_LEFT, 60, 0);
+    lv_label_set_text(label, "ADC");
+
+    lv_obj_t *led;
+    led = lv_led_create(parent);
+    led_adc_btn[0] = led;
+    lv_obj_align(led, LV_ALIGN_TOP_LEFT, 0, 0);
+
+    led = lv_led_create(parent);
+    led_adc_btn[1] = led;
+    lv_obj_align(led, LV_ALIGN_TOP_RIGHT, 0, 0);
+
+    label = lv_label_create(parent);
+    lv_obj_align_to(label, parent, LV_ALIGN_TOP_MID, 0, 0);
+    lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_RIGHT, 0);
+
+    return label;
+}
