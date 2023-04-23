fcb6c5ff51ace9eb93b502d67bac136249cebb37
taorye <taorye@outlook.com>
2023-04-23 10:44:15 +0800

    chore: change name

--- sipeed_support/examples/m0pdock/usb_display/main.c
+++ sipeed_support/examples/m0pdock/usb_display/main.c
@@ -17,9 +17,40 @@ extern void cdc_acm_init(void);
 extern void lcd_display_loop(void);
 extern void shell_init_with_task(struct bflb_device_s *shell);
 
+static uint8_t lcd_dir;
+
 static void vDisplayTimer(TimerHandle_t xTimer)
 {
+    // lcd_set_dir(lcd_dir, 0);
     lcd_display_loop();
+}
+
+static void button_task(void *pvParameters)
+{
+    struct bflb_device_s *gpio;
+    gpio = bflb_device_get_by_name("gpio");
+    bflb_gpio_init(gpio, GPIO_PIN_3, GPIO_INPUT | GPIO_FLOAT | GPIO_SMT_EN | GPIO_DRV_0);
+    bool push_button;
+    uint32_t push_times;
+    uint8_t dir;
+
+    lcd_dir = dir = 0;
+    while (1) {
+        push_times = 0;
+        do {
+            vTaskDelay(pdMS_TO_TICKS(10));
+            push_button = (0 == bflb_gpio_read(gpio, GPIO_PIN_3));
+            push_times += push_button;
+        } while (push_button);
+        if (push_times) {
+            dir = (dir + 1) % 4;
+            lcd_dir = dir;
+            // lcd_set_dir(dir, 0);
+            // printf("Change dir to %u\r\n", dir);
+        }
+    }
+
+    vTaskDelete(NULL);
 }
 
 int main(void)
@@ -46,6 +77,9 @@ int main(void)
     xTimerStart(
         xTimerCreate("display_timer", pdMS_TO_TICKS(10), pdTRUE, NULL, vDisplayTimer),
         pdMS_TO_TICKS(50));
+
+    xTaskHandle button_task_handle;
+    xTaskCreate(button_task, (char *)"producer_task", 512, NULL, configMAX_PRIORITIES - 3, &button_task_handle);
 
     vTaskStartScheduler();
 
--- sipeed_support/examples/m0pdock/usb_display/src/cdc_acm_template.c
+++ sipeed_support/examples/m0pdock/usb_display/src/cdc_acm_template.c
@@ -27,40 +27,37 @@ static const uint8_t cdc_descriptor[] = {
     ///////////////////////////////////////
     /// string1 descriptor
     ///////////////////////////////////////
-    0x14,                       /* bLength */
+    0x0e,                       /* bLength */
     USB_DESCRIPTOR_TYPE_STRING, /* bDescriptorType */
-    'C', 0x00,                  /* wcChar0 */
-    'h', 0x00,                  /* wcChar1 */
-    'e', 0x00,                  /* wcChar2 */
-    'r', 0x00,                  /* wcChar3 */
-    'r', 0x00,                  /* wcChar4 */
-    'y', 0x00,                  /* wcChar5 */
-    'U', 0x00,                  /* wcChar6 */
-    'S', 0x00,                  /* wcChar7 */
-    'B', 0x00,                  /* wcChar8 */
+    'S', 0x00,                  /* wcChar0 */
+    'I', 0x00,                  /* wcChar1 */
+    'P', 0x00,                  /* wcChar2 */
+    'E', 0x00,                  /* wcChar3 */
+    'E', 0x00,                  /* wcChar4 */
+    'D', 0x00,                  /* wcChar5 */
     ///////////////////////////////////////
     /// string2 descriptor
     ///////////////////////////////////////
     0x26,                       /* bLength */
     USB_DESCRIPTOR_TYPE_STRING, /* bDescriptorType */
-    'C', 0x00,                  /* wcChar0 */
-    'h', 0x00,                  /* wcChar1 */
-    'e', 0x00,                  /* wcChar2 */
-    'r', 0x00,                  /* wcChar3 */
-    'r', 0x00,                  /* wcChar4 */
-    'y', 0x00,                  /* wcChar5 */
-    'U', 0x00,                  /* wcChar6 */
-    'S', 0x00,                  /* wcChar7 */
-    'B', 0x00,                  /* wcChar8 */
-    ' ', 0x00,                  /* wcChar9 */
-    'C', 0x00,                  /* wcChar10 */
-    'D', 0x00,                  /* wcChar11 */
-    'C', 0x00,                  /* wcChar12 */
-    ' ', 0x00,                  /* wcChar13 */
-    'D', 0x00,                  /* wcChar14 */
-    'E', 0x00,                  /* wcChar15 */
-    'M', 0x00,                  /* wcChar16 */
-    'O', 0x00,                  /* wcChar17 */
+    'S', 0x00,                  /* wcChar0 */
+    'i', 0x00,                  /* wcChar1 */
+    'p', 0x00,                  /* wcChar2 */
+    'e', 0x00,                  /* wcChar3 */
+    'e', 0x00,                  /* wcChar4 */
+    'd', 0x00,                  /* wcChar5 */
+    ' ', 0x00,                  /* wcChar6 */
+    'M', 0x00,                  /* wcChar7 */
+    'a', 0x00,                  /* wcChar8 */
+    'i', 0x00,                  /* wcChar9 */
+    'x', 0x00,                  /* wcChar10 */
+    'P', 0x00,                  /* wcChar11 */
+    'l', 0x00,                  /* wcChar12 */
+    'a', 0x00,                  /* wcChar13 */
+    'y', 0x00,                  /* wcChar14 */
+    '-', 0x00,                  /* wcChar15 */
+    'U', 0x00,                  /* wcChar16 */
+    '4', 0x00,                  /* wcChar17 */
     ///////////////////////////////////////
     /// string3 descriptor
     ///////////////////////////////////////
@@ -69,13 +66,13 @@ static const uint8_t cdc_descriptor[] = {
     '2', 0x00,                  /* wcChar0 */
     '0', 0x00,                  /* wcChar1 */
     '2', 0x00,                  /* wcChar2 */
-    '2', 0x00,                  /* wcChar3 */
-    '1', 0x00,                  /* wcChar4 */
-    '2', 0x00,                  /* wcChar5 */
-    '3', 0x00,                  /* wcChar6 */
-    '4', 0x00,                  /* wcChar7 */
-    '5', 0x00,                  /* wcChar8 */
-    '6', 0x00,                  /* wcChar9 */
+    '3', 0x00,                  /* wcChar3 */
+    '0', 0x00,                  /* wcChar4 */
+    '4', 0x00,                  /* wcChar5 */
+    '2', 0x00,                  /* wcChar6 */
+    '1', 0x00,                  /* wcChar7 */
+    '1', 0x00,                  /* wcChar8 */
+    '5', 0x00,                  /* wcChar9 */
 #ifdef CONFIG_USB_HS
     ///////////////////////////////////////
     /// device qualifier descriptor
