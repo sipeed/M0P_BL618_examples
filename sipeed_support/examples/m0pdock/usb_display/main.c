74a39927411afe98fdfa5adf0e78d5136977d583
taorye <taorye@outlook.com>
2023-04-28 19:08:44 +0800

    feat: config save to flash

--- sipeed_support/examples/m0pdock/usb_display/main.c
+++ sipeed_support/examples/m0pdock/usb_display/main.c
@@ -2,12 +2,16 @@
 #include "task.h"
 #include "timers.h"
 
-#include "usbh_core.h"
-#include "bflb_mtimer.h"
-#include "bflb_gpio.h"
-#include "bflb_pwm_v2.h"
+#include "bflb_adc.h"
 #include "bflb_clock.h"
+#include "bflb_flash.h"
+#include "bflb_gpio.h"
+#include "bflb_mtimer.h"
+#include "bflb_pwm_v2.h"
+
 #include "board.h"
+
+#include "usbh_core.h"
 #include "shell.h"
 
 #include "lcd.h"
@@ -41,12 +45,15 @@ static void button_task(void *pvParameters)
 {
     struct bflb_device_s *gpio;
     gpio = bflb_device_get_by_name("gpio");
-    bflb_gpio_init(gpio, GPIO_PIN_3, GPIO_INPUT | GPIO_FLOAT | GPIO_SMT_EN | GPIO_DRV_0);
+    /* ADC_CH3 */
+    bflb_gpio_init(gpio, GPIO_PIN_3, GPIO_ANALOG | GPIO_SMT_EN | GPIO_DRV_0);
+    // bflb_gpio_init(gpio, GPIO_PIN_3, GPIO_INPUT | GPIO_FLOAT | GPIO_SMT_EN | GPIO_DRV_0);
     bflb_gpio_init(gpio, GPIO_PIN_2, GPIO_FUNC_PWM0 | GPIO_ALTERNATE | GPIO_PULLDOWN | GPIO_SMT_EN | GPIO_DRV_1);
 
-    bool is_button_pushed;
-    uint32_t pushed_times;
+    uint8_t last_key, distribute_key;
     uint8_t dir;
+    uint8_t brightness;
+    uint16_t brightness_map[] = { 0, 2, 4, 8, 16, 32, 63, 126, 251, 501, 1000 };
 
     struct bflb_device_s *pwm;
     pwm = bflb_device_get_by_name("pwm_v2_0");
@@ -62,22 +69,84 @@ static void button_task(void *pvParameters)
     bflb_pwm_v2_channel_positive_start(pwm, PWM_CH2);
     bflb_pwm_v2_start(pwm);
 
-    lcd_dir = dir = 0;
+    struct bflb_device_s *adc;
+    adc = bflb_device_get_by_name("adc");
+
+    /* adc clock = XCLK / 2 / 32 / 256(16B) = 2.442K */
+    bflb_adc_init(
+        adc,
+        &(struct bflb_adc_config_s){
+            .clk_div = ADC_CLK_DIV_32,
+            .scan_conv_mode = true,
+            .continuous_conv_mode = true,
+            .differential_mode = false,
+            .resolution = ADC_RESOLUTION_16B,
+            .vref = ADC_VREF_3P2V,
+        });
+    bflb_adc_channel_config(
+        adc,
+        (struct bflb_adc_channel_s[]){
+            {
+                .pos_chan = ADC_CHANNEL_3,
+                .neg_chan = ADC_CHANNEL_GND,
+            },
+        },
+        1);
+    bflb_adc_start_conversion(adc);
+
+    typedef struct {
+        uint8_t lcd_dir;
+        uint8_t lcd_bright;
+    } maixplayU4_config_t;
+#define FLASH_BLOCK_SIZE        4096
+#define CONFIG_IN_FLASH_ADDRESS (0x100000 - FLASH_BLOCK_SIZE)
+    maixplayU4_config_t maixplayU4_config;
+    bflb_flash_read(CONFIG_IN_FLASH_ADDRESS, (uint8_t *)&maixplayU4_config, sizeof(maixplayU4_config_t));
+    if (maixplayU4_config.lcd_bright > 10 ||
+        maixplayU4_config.lcd_dir > 3) {
+        maixplayU4_config.lcd_bright = 6;
+        maixplayU4_config.lcd_dir = 0;
+        bflb_flash_erase(CONFIG_IN_FLASH_ADDRESS, FLASH_BLOCK_SIZE);
+        bflb_flash_write(CONFIG_IN_FLASH_ADDRESS, (uint8_t *)&maixplayU4_config, sizeof(maixplayU4_config_t));
+    }
+
+    dir = lcd_dir = maixplayU4_config.lcd_dir;
+    brightness = maixplayU4_config.lcd_bright;
+
+    bflb_pwm_v2_channel_set_threshold(pwm, PWM_CH2, 100, 100 + brightness_map[brightness]);
+    distribute_key = last_key = 0;
     while (1) {
-        pushed_times = 0;
+        distribute_key = 0;
         do {
             vTaskDelay(pdMS_TO_TICKS(10));
-            is_button_pushed = (0 == bflb_gpio_read(gpio, GPIO_PIN_3));
-            pushed_times += is_button_pushed;
-        } while (is_button_pushed);
-        if (pushed_times) {
-            // dir = (dir + 1) % 4;
-            // lcd_dir = dir;
-            // printf("Changing dir to %u\r\n", dir);
-            dir = (dir + 1) % 11;
-            uint16_t map[] = { 0, 2, 4, 8, 16, 32, 63, 126, 251, 501, 1000 };
-            bflb_pwm_v2_channel_set_threshold(pwm, PWM_CH2, 100, 100 + map[dir]);
-            printf("Changing pwm duty to %u%\r\n", dir * 10);
+            uint32_t raw_val = bflb_adc_read_raw(adc);
+            uint8_t tmp_val = ((raw_val & 0xffff) >> 2) >> 7;
+            // printf("tmp_val: %u\r\n", tmp_val);
+            uint8_t key = (tmp_val < 60) + (tmp_val < 25);
+            // printf("key: %u\r\n", key);
+            if (key != last_key) {
+                if (0 == last_key) {
+                    distribute_key = key;
+                }
+            }
+            last_key = key;
+        } while (!distribute_key);
+
+        if (2 == distribute_key) {
+            dir = (dir + 1) % 4;
+            lcd_dir = dir;
+            printf("Changing dir to %u\r\n", dir);
+            maixplayU4_config.lcd_dir = lcd_dir;
+        } else if (1 == distribute_key) {
+            brightness = (brightness + 1) % 11;
+            bflb_pwm_v2_channel_set_threshold(pwm, PWM_CH2, 100, 100 + brightness_map[brightness]);
+            printf("Changed pwm duty to %u%\r\n", brightness * 10);
+            maixplayU4_config.lcd_bright = brightness;
+        }
+
+        {
+            bflb_flash_erase(CONFIG_IN_FLASH_ADDRESS, FLASH_BLOCK_SIZE);
+            bflb_flash_write(CONFIG_IN_FLASH_ADDRESS, (uint8_t *)&maixplayU4_config, sizeof(maixplayU4_config_t));
         }
     }
 
